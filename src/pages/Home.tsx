1f5123a9c65bb2a81c8a7a1b21a471c5bbe7f49a
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-07-14 15:26:06 +0900

    :sparkles: City list 리스트업

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,32 @@
+import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 
-import { sampleState } from "@/state/sample";
+import fetcher from "@/api/fetcher";
+import { cityList } from "@/state/common";
 
 const Home = () => {
-  const [sample, setSample] = useRecoilState(sampleState);
+  const [cities, setCities] = useRecoilState(cityList);
+
+  const getCityList = async () => {
+    const res: any = await fetcher("get", "/data/citylist.json", {});
+    if (res.data.length === 0) {
+      console.log("no data");
+    }
+    setCities(res.data);
+    console.log("newCitys", res.data[0].name);
+  };
+
+  useEffect(() => {
+    getCityList();
+  }, []);
 
   return (
     <>
       <h1>Home</h1>
-      <p>{String(sample)}</p>
+      {cityList &&
+        cities.map((city: any, id: number) => {
+          return <div key={id}>{city.name}</div>;
+        })}
     </>
   );
 };
---
+++ src/state/common.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const cityList = atom({
+  key: "cityList",
+  default: [],
+});
