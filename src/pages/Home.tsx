803f0aeef9fc5022c62f6ca102669938d5f82607
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-07-15 04:42:51 +0900

    :art: Home 코드 개선 (상태, 함수 성능 향상)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,43 +10,77 @@ import { cityList } from "@/state/common";
 const Home = () => {
   const [cities, setCities] = useRecoilState<any[]>(cityList as any);
   const [randomCity, setRandomCity] = useState<any | null>(null);
+  const [lat, setLat] = useState<number | null>(null);
+  const [lon, setLon] = useState<number | null>(null);
+  const [weather, setWeather] = useState<any | null>(null);
 
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  // MARK: 도시 리스트 받아오기
   const getCityList = async () => {
-    try {
-      const res: any = await fetcher("get", `/data/citylist.json`, {});
+    const res: any = await fetcher("get", `/data/citylist.json`, {});
+    if (res.data.length === 0) console.log("no data");
+    else setCities(res.data);
+  };
 
+  // MARK: 현위치 (위도, 경도) 받아오기
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      setLat(latitude);
+      setLon(longitude);
+    });
+  };
+
+  // MARK: 위도, 경도를 url 파라미터로 넣어 날씨 정보 받아오기
+  const getWeather = async () => {
+    if (lat && lon) {
+      const res: any = await fetcher("get", url, {});
       if (res.data.length === 0) console.log("no data");
-      else setCities(res.data);
-
-      // console.log("newCities", res.data[0].name);
-    } catch (error) {
-      console.error(error);
+      else {
+        setWeather(res.data);
+        console.log("weather:", weather.weather[0].description);
+      }
     }
   };
 
+  // MARK: 도시, 위도, 경도 정보가 변경될 때마다 렌더링.
   useEffect(() => {
-    try {
-      if (!cities || cities.length === 0) {
-        getCityList();
-      } else {
-        const selectedRandomCity = cities[Math.floor(Math.random() * cities.length)];
-        setRandomCity(selectedRandomCity || null);
-      }
-    } catch (error) {
-      console.error(error);
+    // MARK: 도시 리스트 및 랜덤 도시 받아오기
+    if (!cities || cities.length === 0) getCityList();
+    else {
+      const selectedRandomCity = cities[Math.floor(Math.random() * cities.length)];
+      setRandomCity(selectedRandomCity || null);
     }
-  }, [cities]);
 
-  console.log("randomCity", randomCity);
+    // MARK: 현위치 받은 이후 날씨 받기
+    const fetchData = async () => {
+      getCurrentLocation();
+      console.log("현재 위치:", lat, lon);
+
+      await getWeather();
+    };
+
+    fetchData();
+  }, [cities, lat, lon]);
 
   return (
     <HomeContainer>
       <TitleContainer>
-        오늘
-        {randomCity && (
-          <StyledLink to={`${PATH.DETAIL}/${randomCity?.id}`}>{randomCity?.name}</StyledLink>
+        {weather && randomCity && (
+          <>
+            <TodaysWeatherTitle>
+              오늘
+              <StyledLink to={`${PATH.DETAIL}/${randomCity?.id}`}>{randomCity?.name}</StyledLink>의
+              날씨는?
+            </TodaysWeatherTitle>
+            <TemperatureTitle>{weather.main.temp}°C</TemperatureTitle>
+            <WeatherTitle>{weather.weather[0].main}</WeatherTitle>
+          </>
         )}
-        의 날씨는?
       </TitleContainer>
 
       <CityListContainer>
@@ -74,17 +108,30 @@ const HomeContainer = styled.div`
 `;
 
 const TitleContainer = styled.div`
-  margin: 4em 2em;
-  font-size: 2em;
-  font-weight: bold;
+  margin: 6em 0 4em 0;
+  font-size: 1.5em;
+  font-weight: 600;
   text-align: center;
   white-space: nowrap;
 `;
 
+const TodaysWeatherTitle = styled.div`
+  margin-bottom: 1em;
+`;
+
 const StyledLink = styled(Link)`
-  margin: 0 0.5em;
+  margin: 0 0.4em;
+  font-weight: 700;
   text-decoration: underline;
 `;
+
+const TemperatureTitle = styled.h2`
+  margin: 0.5em 0;
+  font-size: 2.5em;
+  font-weight: 700;
+`;
+
+const WeatherTitle = styled.h4``;
 
 const CityListContainer = styled.div`
   display: grid;
