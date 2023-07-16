8e320de9d9758a7a93ca8c129b272ddd7f2e4293
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-07-16 22:59:38 +0900

    :adhesive_bandage: Simple fix

--- src/state/common.ts
+++ src/state/common.ts
@@ -68,9 +68,49 @@ export const searchHistory = atom<History[]>({
   default: [],
 });
 
-export const weatherInfo = atom<Weather | null>({
+export const weatherInfo = atom<Weather>({
   key: "weather",
-  default: null,
+  default: {
+    clouds: { all: 0 },
+    coord: {
+      lat: 0,
+      lon: 0,
+    },
+    dt: 0,
+    id: 0,
+    main: {
+      feels_like: 0,
+      grnd_level: 0,
+      humidity: 0,
+      pressure: 0,
+      sea_level: 0,
+      temp: 0,
+      temp_max: 0,
+      temp_min: 0,
+    },
+    name: "",
+    sys: {
+      country: "",
+      sunrise: 0,
+      sunset: 0,
+      type: 0,
+    },
+    timezone: 0,
+    visibility: 0,
+    weather: [
+      {
+        description: "",
+        icon: "",
+        id: 0,
+        main: "",
+      },
+    ],
+    wind: {
+      deg: 0,
+      gust: 0,
+      speed: 0,
+    },
+  },
 });
 
 export const isOpen = atom<boolean>({
